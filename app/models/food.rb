8815ecc6165f893fc7d1ba4a56a6aa5dd4b28e8e
fgolfc <cf.ts.fujimoto@gmail.com>
2023-01-12 21:44:38 +0900

    4times

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,7 +1,5 @@
 class Food < ApplicationRecord
-  has_many :order_food
+  has_and_belongs_to_many :orders
+  has_many :order_foods
   belongs_to :shop
-  has_one :chinese_food
-  has_one :italian_food
-  has_one :japanese_food
 end
